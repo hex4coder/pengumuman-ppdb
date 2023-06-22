0c9da7b7dfb2a6e36caaaec4ed1be4c7201af906
hex4coder <the.programmer.luyo@gmail.com>
2023-06-22 15:37:15 +0800

    Perbaikan bugs

---
+++ src/core/init.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'core/db.php';
+
+class UA_Init
+{
+
+
+    public static function rebuildDatabase()
+    {
+    }
+
+    public static function recreateTableStudents()
+    {
+        $ret = false;
+        $db = new UA_Database();
+        $p = mysqli_query($db->connection(), "CREATE TABLE IF NOT EXITS students(
+            nomor VARCHAR(13),
+            nama VARCHAR(200),
+            
+        )");
+        $q = mysqli_query($db->connection(), "TRUNCATE TABLE students");
+        $ret = $q != false;
+        $db->closeConnection();
+        return $ret;
+    }
+
+    public static function insertAllDataToTableStudents()
+    {
+        $ret 
+    }
+}
