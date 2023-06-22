b78f0ae10e0a64f27c43ebcf04c55a8068023905
hex4coder <the.programmer.luyo@gmail.com>
2023-06-22 11:08:40 +0800

    Penambahan css custom

--- index.php
+++ index.php
@@ -11,8 +11,11 @@
 
     <!-- Bootstrap JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
-<body>
-    <img src="assets/img/welcome.png" alt="welcome screen">
+<body class="background">
+    
 </body>
 </html>
