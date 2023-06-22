bb5d57c1be1ff46b3809c207a05235e0c70da51c
hex4coder <the.programmer.luyo@gmail.com>
2023-06-22 11:00:13 +0800

    Penambahan gambar welcome screen

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <!-- Bootstrap JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
-<body class="bg-primary">
-    <h1>Selamat Datang!</h1>
+<body>
+    <img src="assets/img/welcome.png" alt="welcome screen">
 </body>
 </html>
